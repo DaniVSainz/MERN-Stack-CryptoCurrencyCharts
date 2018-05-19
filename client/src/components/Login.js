17639cd4022d466c42f518441413011619bfef29
danivsainz <danivsainz@gmail.com>
2018-05-19 15:27:12 -0400

    begin form setup

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import { reduxForm, Field } from "redux-form";
 
 class Login extends Component {
   render() {
     return (
       <div>
         Login Page
+        <form onSubmit={this.props.handleSubmit((values)=> console.log(values))}>
+          <Field component="input" label="email" name="email" type="text"></Field>
+          <Field component="input" label="password" name="password" type="text"></Field>
+          <button type="submit" className="teal btn-flat right white-text">
+            Next
+            <i className="material-icons right">done</i>
+          </button>
+        </form>
       </div>
     );
   }
 }
 
-export default Login;
+export default reduxForm({
+  form: "loginForm",
+  destroyOnUnmount:false
+})(Login);
