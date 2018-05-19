d39bbee5749539f50efa5ed84ee379eb27962b6f
danivsainz <danivsainz@gmail.com>
2018-05-18 21:31:49 -0400

    add dependancies etc

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,10 +3,7 @@ import { BrowserRouter, Route } from "react-router-dom";
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 
-import Header from './Header';
-import Landing from './Landing';
-import Dashboard from './Dashboard';
-import SurveyNew from './surveys/SurveyNew';
+
 
 class App extends Component {
 
@@ -19,10 +16,11 @@ class App extends Component {
       <div className="container">
         <BrowserRouter>
           <div>
-            <Header></Header>
+            Hi
+            {/* <Header></Header>
             <Route path="/" component={Landing} exact></Route>
             <Route path="/surveys" component={Dashboard} exact></Route>
-            <Route path="/surveys/new" component={SurveyNew}></Route>
+            <Route path="/surveys/new" component={SurveyNew}></Route> */}
           </div>
         </BrowserRouter>
       </div>
--- server.js
+++ server.js
@@ -10,61 +10,41 @@ require('dotenv').config()
 
 
 mongoose.Promise = require('bluebird');
-
-//Works easier with deploys and git not having to change stuff and just using .env vars
 mongoose.connect(process.env.mongoUrl);
-
-
-// On Connection
 mongoose.connection.on('connected', () => {
   console.log('Connected to Database '+ process.env.mongoUrl);
 });
-// On Error
 mongoose.connection.on('error', (err) => {
   console.log('Database error '+err);
 });
 
 const app = express();
-
-const users = require('./routes/users');
-const confirmation = require('./routes/confirmation');
-
-//The port number inside of bin/www takes precendence
-// Port Number
-const port = process.env.PORT || 8080;
-
-
 app.use(logger('dev'));
-
-
 // CORS Middleware
 app.use(cors());
-
-// Set Static Folder
-app.use(express.static(path.join(__dirname, 'angular-src/dist')));
-
 // Body Parser Middleware
 app.use(bodyParser.json());
-
 // Passport Middleware
 app.use(passport.initialize());
 app.use(passport.session());
-
 require('./config/passport')(passport);
-
+//Routes
+const users = require('./routes/users');
+const confirmation = require('./routes/confirmation');
 app.use('/users', users);
 app.use('/confirmation', confirmation);
 
+app.get('/', (req,res)=>{
+  res.send({msg:'Test Route'})
+})
 
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
 
-
-// Index Route
-app.get('/', (req, res) => {
-  res.send('invaild endpoint');
-});
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './angular-src/dist/index.html'));
-});
+  const path = require('path');
+  app.get('*', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 module.exports = app;
