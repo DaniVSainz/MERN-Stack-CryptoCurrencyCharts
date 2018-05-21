05f37644af599cc16f71fd623fa9e0c666afc3e5
danivsainz <danivsainz@gmail.com>
2018-05-21 08:07:36 -0400

    begin new form setup

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,13 +1,48 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200,
+  },
+  menu: {
+    width: 200,
+  },
+});
 
 class Register extends Component {
+  
   render() {
+    const { classes } = this.props;
+
     return (
       <div>
         Hello from register
+        <div>
+          <TextField
+            id="with-placeholder"
+            label="With placeholder"
+            placeholder="Placeholder"
+            className={classes.textField}
+            margin="normal"
+          />
+        </div>
       </div>
     );
   }
 }
 
-export default Register;
+Register.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Register);
