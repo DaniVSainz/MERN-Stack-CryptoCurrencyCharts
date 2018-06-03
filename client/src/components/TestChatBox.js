8f52e14cbc9ae9c93e503a0f49da9715934befd6
danivsainz <danivsainz@gmail.com>
2018-06-03 10:43:00 -0400

    can emite msgs to server

--- client/src/components/TestChatBox.js
+++ client/src/components/TestChatBox.js
@@ -3,20 +3,29 @@ import Draggable from 'react-draggable';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 import socket from '../utils/socket'
+import TextField from '@material-ui/core/TextField';
 
 class ChatBox extends Component {
 
   state={
     socket: socket(),
+    textFieldValue: ''
   }
 
   emitMsg(){
-    console.log(this.props)
-    this.socket.emit('my other event', 'Hi from react')
+    this.state.socket.emitTest(this.state.textFieldValue)
+    this.setState({textFieldValue: ''});
   }
 
   canJoinChat(){
     if(this.props.auth.user.user){}
+  }
+
+  async handleTextFieldChange(e){
+    await this.setState({
+      textFieldValue: e.target.value
+    });
+    console.log(this.state.textFieldValue)
   }
 
   render() {
@@ -38,7 +47,15 @@ class ChatBox extends Component {
             <div style={{border:'1px solid black', width:'fit-content'}}>
               <div className="handle">Drag from here</div>
               <div>This readme is really dragging on...</div>
-              <button onClick={()=> this.emitMsg()}>Connect to chat</button>
+              <button onClick={this.emitMsg.bind(this)}>Connect to chat</button>
+              <TextField label="Message...." 
+                value={this.state.textFieldValue}
+                onChange={this.handleTextFieldChange.bind(this)}
+                onSubmit={()=> this.emitMsg()}
+                style={{color:'black'}}
+              >
+                {this.state.textFieldValue}
+              </TextField>
             </div>
           </Draggable>
         </div>
--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -3,6 +3,11 @@ const io = require('socket.io-client')
 export default function () {
   const socket = io.connect('http://localhost:5000');
 
-  
+  function emitTest(msg) {
+    socket.emit('my other event', msg)
+  }
 
+  return{
+    emitTest
+  }
 }
