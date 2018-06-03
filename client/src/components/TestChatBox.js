492fb0ac2895b44a7573eadaf3cb98bad2c7cefe
danivsainz <danivsainz@gmail.com>
2018-06-02 20:36:00 -0400

    begin implementing websockets, add react draggable

--- client/src/components/TestChatBox.js
+++ client/src/components/TestChatBox.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
+import Draggable from 'react-draggable';
 
 class ChatBox extends Component {
   state = {  }
+
   render() {
     return (
-      <div>
-        <div>
+      <div style={{backgroundColor: 'white'}}>
+        <div >
           The best chat box around
+        </div>
+        <div>
+          <Draggable
+          axis="x"
+          handle=".handle"
+          defaultPosition={{x: 0, y: 0}}
+          position={null}
+          grid={[25, 25]}
+          onStart={this.handleStart}
+          onDrag={this.handleDrag}
+          onStop={this.handleStop}
+          
+          >
+            <div style={{border:'1px solid black', width:'fit-content'}}>
+              <div className="handle">Drag from here</div>
+              <div>This readme is really dragging on...</div>
+            </div>
+          </Draggable>
         </div>
       </div>
     );
