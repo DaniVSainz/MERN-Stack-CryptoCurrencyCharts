402f7f0e2e009608b62cb8acc70e7c62ab5ee292
danivsainz <danivsainz@gmail.com>
2018-06-04 08:22:13 -0400

    add .env vars

--- client/src/actions/Sockets.js
+++ client/src/actions/Sockets.js
@@ -1,7 +1,13 @@
 import {SOCKET_CONNECT,SOCKET_MSGS} from './types';
 import {store} from '../index'
 const io = require('socket.io-client');
-const socket = io.connect('http://localhost:5000');
+let socket;
+if(process.env.REACT_APP_ENV === 'dev'){
+   socket = io.connect('http://localhost:5000');
+}else{
+   socket = io.connect('http://159.89.48.60:5000');
+}
+
 
 socket.on('connected', (msg) => {
   store.dispatch({ type: SOCKET_CONNECT, payload: msg });
